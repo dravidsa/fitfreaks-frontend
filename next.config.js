f7275c65887d3601b57169cb8be71d045234c35b
“dravidsa” <dravidsa@hotmail.com>
2024-07-31 22:16:09 +0530

    made changes related to terms and conditions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['dompurify'] = path.resolve(__dirname, 'node_modules/dompurify/dist/purify.es.js');
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
